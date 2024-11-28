424032611841f441fa2e6566a88e51b71e7ba2a6
MatthewDent <N00220082@iadt.ie>
2024-11-28 11:37:17 +0000

    created folders for the code for doctors appointments and patients as well as routes in the app.js and imports

--- src/App.js
+++ src/App.js
@@ -9,6 +9,20 @@ import ProtectedRoute from './components/ProtectedRoute'
 import Create from './pages/festivals/Create';
 import Edit from './pages/festivals/Edit'
 import { AuthProvider } from "./utils/useAuth";
+import DoctorsIndex from './pages/doctors/Index';
+import CreateDoctor from './pages/doctors/Create';
+import EditDoctor from './pages/doctors/Edit';
+import ViewDoctor from './pages/doctors/View';
+
+import PatientsIndex from './pages/patients/Index';
+import CreatePatient from './pages/patients/Create';
+import EditPatient from './pages/patients/Edit';
+import ViewPatient from './pages/patients/View';
+
+import AppointmentsIndex from './pages/appointments/Index';
+import CreateAppointment from './pages/appointments/Create';
+import EditAppointment from './pages/appointments/Edit';
+import ViewAppointment from './pages/appointments/View';
 
 const App = () => {
 
@@ -30,6 +44,25 @@ const App = () => {
                     </Route>
                     <Route path='/login' element={<LoginForm />} />
                     <Route path='/register' element={<RegisterForm />} />
+
+                    {/* Clinic routes */}
+                    {/* Doctors Routes */}
+                    <Route path="/doctors" element={<DoctorsIndex />} />
+                    <Route path="/doctors/create" element={<CreateDoctor />} />
+                    <Route path="/doctors/:id/edit" element={<EditDoctor />} />
+                    <Route path="/doctors/:id" element={<ViewDoctor />} />
+
+                    {/* Patients Routes */}
+                    <Route path="/patients" element={<PatientsIndex />} />
+                    <Route path="/patients/create" element={<CreatePatient />} />
+                    <Route path="/patients/:id/edit" element={<EditPatient />} />
+                    <Route path="/patients/:id" element={<ViewPatient />} />
+
+                    {/* Appointments Routes */}
+                    <Route path="/appointments" element={<AppointmentsIndex />} />
+                    <Route path="/appointments/create" element={<CreateAppointment />} />
+                    <Route path="/appointments/:id/edit" element={<EditAppointment />} />
+                    <Route path="/appointments/:id" element={<ViewAppointment />} />
                 </Routes>
             </Router>
         </AuthProvider>

3a59120ee70461da931a8959e904881fed6be12e
MatthewDent <N00220082@iadt.ie>
2024-12-16 18:14:42 +0000

    login and register designed with scss and bootstrap

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,95 +1,51 @@
-// import axios from 'axios';
-// import { useState } from 'react';
-// import { useNavigate } from 'react-router-dom';
-// import { useAuth } from '../utils/useAuth';
-
-// const LoginForm = () => {
-//     const navigate = useNavigate();
-//     const { login } = useAuth();
-
-//     const [form, setForm] = useState({
-//         email: '',
-//         password: ''
-//     });
-
-//     const handleSubmit = async (e) => {
-//         e.preventDefault();
-
-//         try {
-//             await login(form.email, form.password);
-//             navigate('/'); // Navigate only after login is successful
-//         } catch (err) {
-//             console.error('Login failed:', err);
-//             alert('Login failed, please check your credentials');
-//         }
-//     };
-
-//     const handleChange = (e) => {
-//         setForm({
-//             ...form,
-//             [e.target.name]: e.target.value
-//         });
-//     };
-
-//     return (
-//         <form onSubmit={handleSubmit}>
-//             <input
-//                 onChange={handleChange}
-//                 value={form.email}
-//                 type='email'
-//                 name='email'
-//                 placeholder='joe.bloggs@email.com'
-//             />
-//             <input
-//                 onChange={handleChange}
-//                 value={form.password}
-//                 type='password'
-//                 name='password'
-//             />
-//             <button type='submit'>Submit</button>
-//         </form>
-//     );
-// };
-
-// export default LoginForm;
-
-import axios from 'axios'
-import { useState } from 'react'
+import axios from 'axios';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../utils/useAuth';
+import '../styles/login.scss'; // Import your custom SCSS file
 
 const LoginForm = () => {
     const navigate = useNavigate();
-    const {login} = useAuth();
+    const { login } = useAuth();
 
     const [form, setForm] = useState({
         email: '',
         password: ''
-    })
+    });
 
     const handleSubmit = (e) => {
-        // The form will cause a refresh by default. We don't want that, because our state will disappear.
         e.preventDefault();        
 
         login(form.email, form.password)
-           
-        navigate('/')
-    }
+            .then(() => {
+                navigate('/');
+            })
+            .catch((err) => {
+                console.error('Login failed:', err);
+                alert('Login failed, please check your credentials');
+            });
+    };
 
     const handleChange = (e) => {
-        setForm(({
+        setForm({
             ...form,
             [e.target.name]: e.target.value
-        }))
-    }
+        });
+    };
 
     return (
-        <form>
-            <input onChange={handleChange} value={form.email} type='email' name='email' placeholder='joe.bloggs@email.com'></input>
-            <input onChange={handleChange} value={form.password} type='password' name='password'></input>
-            <button onClick={handleSubmit}>Submit</button>
+        <form className="login-form" onSubmit={handleSubmit}>
+            <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input onChange={handleChange} value={form.email} type="email" name="email" id="email" placeholder="joe.bloggs@email.com" />
+            </div>
+            <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <input onChange={handleChange} value={form.password} type="password" name="password" id="password" />
+            </div>
+            <button className="submit-button" type="submit">Submit</button>
         </form>
-    )
+    );
 }
 
-export default LoginForm
+export default LoginForm;
